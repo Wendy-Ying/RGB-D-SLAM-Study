6883e819368069a73d580307b835527b71fa3c3e
Wendy-Ying <143325815+Wendy-Ying@users.noreply.github.com>
2024-11-22 23:57:13 +0800

    Add files via upload

---
+++ build/src/CMakeFiles/slambase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../lib/libslambase.a"
+  "../../lib/libslambase.pdb"
+  "CMakeFiles/slambase.dir/slamBase.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slambase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/visualOdometry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualOdometry.dir/visualOdometry.cpp.o"
+  "visualOdometry"
+  "visualOdometry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualOdometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
