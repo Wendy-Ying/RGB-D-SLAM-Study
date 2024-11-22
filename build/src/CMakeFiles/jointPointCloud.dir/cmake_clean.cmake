db638448397cc63cd657bb7bf6ba0cdbcf1deb6c
Wendy-Ying <143325815+Wendy-Ying@users.noreply.github.com>
2024-11-22 23:59:08 +0800

    Add files via upload

---
+++ build/src/CMakeFiles/jointPointCloud.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jointPointCloud.dir/jointPointCloud.cpp.o"
+  "jointPointCloud"
+  "jointPointCloud.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jointPointCloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/jointPointCloud1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jointPointCloud1.dir/jointPointCloud1.cpp.o"
+  "jointPointCloud1"
+  "jointPointCloud1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jointPointCloud1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
